5c2bea892ecb5c7ee5a196435836cc39a2b7bc4b
Rafał Świerczek <rafal.swierczek85@gmail.com>
2018-08-28 18:17:51 +0200

    Spring Data JPA Query Methods

--- src/main/java/guru/springframework/spring5recipeapp/guru/springframework/spring5recipeapp/controllers/IndexController.java
+++ src/main/java/guru/springframework/spring5recipeapp/guru/springframework/spring5recipeapp/controllers/IndexController.java
@@ -1,13 +1,32 @@
 package guru.springframework.spring5recipeapp.guru.springframework.spring5recipeapp.controllers;
 
+import guru.springframework.spring5recipeapp.domain.Category;
+import guru.springframework.spring5recipeapp.domain.UnitOfMeasure;
+import guru.springframework.spring5recipeapp.repositories.CategoryRepository;
+import guru.springframework.spring5recipeapp.repositories.UnitOfMeasureRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 public class IndexController {
 
+    private final CategoryRepository categoryRepository;
+    private final UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
     @RequestMapping({"","/", "index", "index.html"})
     public String index(){
+        Optional<Category> category = categoryRepository.findByDescription("American");
+
+        Optional<UnitOfMeasure> unitOfMeasure = unitOfMeasureRepository.findByDescription("Teaspoon");
+        System.out.println("category "+category.get().getId());
+        System.out.println("unitOfMeasure "+unitOfMeasure.get().getId());
         return "index";
     }
 }
