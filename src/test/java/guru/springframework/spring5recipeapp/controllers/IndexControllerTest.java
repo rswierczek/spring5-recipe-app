b8ef15518af032f6c5b039c3e5fe42418173a28b
Rafał Świerczek <rafal.swierczek85@gmail.com>
2018-08-29 17:12:34 +0200

    argumentCaptor

--- src/test/java/guru/springframework/spring5recipeapp/controllers/IndexControllerTest.java
+++ src/test/java/guru/springframework/spring5recipeapp/controllers/IndexControllerTest.java
@@ -1,17 +1,20 @@
 package guru.springframework.spring5recipeapp.controllers;
 
+import guru.springframework.spring5recipeapp.domain.Recipe;
 import guru.springframework.spring5recipeapp.services.RecipeService;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.ui.Model;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import static org.junit.Assert.assertEquals;
-import static org.mockito.ArgumentMatchers.anySet;
 import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 public class IndexControllerTest {
     private IndexController indexController;
@@ -30,11 +33,30 @@ public class IndexControllerTest {
 
     @Test
     public void index() throws Exception {
-        String viewName = indexController.index(model);
 
+        //given
+        Set<Recipe> recipes = new HashSet<>();
+        Recipe recipe1 = new Recipe();
+        recipe1.setId(1L);
+        recipes.add(recipe1);
+        Recipe recipe2 = new Recipe();
+        recipe2.setId(2L);
+        recipes.add(recipe2);
+
+        //when
+        when(recipeService.getRecipes()).thenReturn(recipes);
+
+        String viewName = indexController.index(model);
+        ArgumentCaptor<Set<Recipe>> argumentCaptor = ArgumentCaptor.forClass(Set.class);
+
+        //then
         assertEquals("index", viewName);
         verify(recipeService, times(1)).getRecipes();
-        verify(model, times(1)).addAttribute(eq("recipes"), anySet());
+        verify(model, times(1)).addAttribute(eq("recipes"), argumentCaptor.capture());
+
+        Set<Recipe> recipesCaptor = argumentCaptor.getValue();
+        assertEquals(2, recipesCaptor.size());
+
     }
 
 }
